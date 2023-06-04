2bfc368f1f9cc5f0d9a6661c8d61d97d4f6dcead
JYZ-XiaoNB <93431491+JYZ-XiaoNB@users.noreply.github.com>
2023-06-04 23:02:34 +0800

    MainFrame.java

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -1,20 +1,202 @@
-package ui;
+package com.ascent.ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 /**
  * 艾斯医药主框架界面
- * 这是一个 Java GUI 程序，主要包含了一个名为 MainFrame 的类，它继承自 JFrame。MainFrame 类中实现了一个基本的窗口界面，
- * 有一个 JTabbedPane 组件和一个 ProductPanel 组件。同时，还包含了一个菜单栏，其中包括一个“文件”菜单和一个“帮助”菜单。
- * <p>
- * 在“文件”菜单中，有打开、保存和退出等选项。
- * <p>
- * 在“帮助”菜单中，有关于选项。
- * <p>
- * 此外，还实现了一些内部类，用于处理菜单项、窗口关闭事件和外观选择事件等。
- *
- * @author cjc
+ * @author ascent
  * @version 1.0
  */
+@SuppressWarnings("serial")
 public class MainFrame extends JFrame {
+
+	/**
+	 * tabbed pane组件
+	 */
+	protected JTabbedPane tabbedPane;
+
+	/**
+	 * 产品 panel
+	 */
+	protected ProductPanel productPanel;
+
+	/**
+	 * 默认构造方法
+	 */
+	public MainFrame() {
+
+		setTitle("欢迎使用AscentSys应用! ");
+
+		Container container = this.getContentPane();
+		container.setLayout(new BorderLayout());
+
+		tabbedPane = new JTabbedPane();
+
+		productPanel = new ProductPanel(this);
+		tabbedPane.addTab("药品", productPanel);
+
+		container.add(BorderLayout.CENTER, tabbedPane);
+
+		JMenuBar myMenuBar = new JMenuBar();
+		
+		JMenu fileMenu = new JMenu("文件");
+		JMenu openMenu = new JMenu("打开");
+		
+		JMenuItem AddProducts = new JMenuItem("添加产品（管理员）");
+		openMenu.add(AddProducts);
+		
+		AddProducts.addActionListener(new AddProductsListener());
+		
+		
+		JMenuItem localMenuItem = new JMenuItem("本地硬盘...");
+		openMenu.add(localMenuItem);
+
+		JMenuItem networkMenuItem = new JMenuItem("网络...");
+		openMenu.add(networkMenuItem);
+
+		JMenuItem webMenuItem = new JMenuItem("互联网...");
+		openMenu.add(webMenuItem);
+		fileMenu.add(openMenu);
+
+		JMenuItem saveMenuItem = new JMenuItem("保存");
+		fileMenu.add(saveMenuItem);
+
+		JMenuItem exitMenuItem = new JMenuItem("退出");
+		fileMenu.add(exitMenuItem);
+
+		myMenuBar.add(fileMenu);
+
+		exitMenuItem.addActionListener(new ExitActionListener());
+
+		setupLookAndFeelMenu(myMenuBar);
+
+		JMenu helpMenu = new JMenu("帮助");
+		JMenuItem aboutMenuItem = new JMenuItem("关于");
+		JMenuItem teachMenuItem = new JMenuItem("使用方法");
+		helpMenu.add(aboutMenuItem);
+		helpMenu.add(teachMenuItem);
+
+		myMenuBar.add(helpMenu);
+
+		aboutMenuItem.addActionListener(new AboutActionListener());
+		
+		teachMenuItem.addActionListener(new TeachActionListener());
+
+		this.setJMenuBar(myMenuBar);
+
+		setSize(500, 400);
+		setLocation(100, 100);
+
+		this.addWindowListener(new WindowCloser());
+
+		fileMenu.setMnemonic('f');
+		exitMenuItem.setMnemonic('x');
+		helpMenu.setMnemonic('h');
+		aboutMenuItem.setMnemonic('a');
+
+		// 设定快捷键
+		exitMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
+				ActionEvent.CTRL_MASK));
+
+		saveMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,
+				ActionEvent.CTRL_MASK));
+
+		aboutMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,
+				ActionEvent.CTRL_MASK));
+	}
+
+	/**
+	 * 设定和选择外观
+	 */
+	protected void setupLookAndFeelMenu(JMenuBar theMenuBar) {
+
+		UIManager.LookAndFeelInfo[] lookAndFeelInfo = UIManager
+				.getInstalledLookAndFeels();
+		JMenu lookAndFeelMenu = new JMenu("选项");
+		JMenuItem anItem = null;
+		LookAndFeelListener myListener = new LookAndFeelListener();
+
+		try {
+			for (int i = 0; i < lookAndFeelInfo.length; i++) {
+				anItem = new JMenuItem(lookAndFeelInfo[i].getName() + " 外观");
+				anItem.setActionCommand(lookAndFeelInfo[i].getClassName());
+				anItem.addActionListener(myListener);
+
+				lookAndFeelMenu.add(anItem);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		theMenuBar.add(lookAndFeelMenu);
+	}
+
+	/**
+	 * 退出方法.
+	 */
+	public void exit() {
+		setVisible(false);
+		dispose();
+		System.exit(0);
+	}
+
+	/**
+	 * "退出"事件处理内部类.
+	 */
+	class ExitActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			exit();
+		}
+	}
+
+	/**
+	 * 处理"关闭窗口"事件的内部类.
+	 */
+	class WindowCloser extends WindowAdapter {
+
+		/**
+		 * let's call our exit() method defined above
+		 */
+		public void windowClosing(WindowEvent e) {
+			exit();
+		}
+	}
+
+	/**
+	 * 处理"外观"选择监听器的内部类
+	 */
+	class LookAndFeelListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			String className = event.getActionCommand();
+			try {
+				UIManager.setLookAndFeel(className);
+				SwingUtilities.updateComponentTreeUI(MainFrame.this);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 处理"关于"菜单监听器的内部类
+	 */
+	class AboutActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			String msg = "超值享受!";
+			JOptionPane.showMessageDialog(MainFrame.this, msg);
+		}
+	}
+	class TeachActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			String msg = "根据选择类别选购药品，点击购买后，加入购物车再统一结算";
+			JOptionPane.showMessageDialog(MainFrame.this, msg);
+		}
+	}
+	class AddProductsListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			AdminLogin registFrame = new AdminLogin();
+			registFrame.setVisible(true);
+		}
+	}
 }
